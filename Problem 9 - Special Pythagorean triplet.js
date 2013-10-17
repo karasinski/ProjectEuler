d1e6f6e225ae5e6e8349355f200934b0fa6ec035
Johnny Karasinski <karasinski@gmail.com>
2013-10-16 22:46:52 -0700

    add solutions for problem 9, 10

---
+++ Problem 9 - Special Pythagorean triplet.js
@@ -0,0 +1,15 @@
+function triplet() {
+    var a;
+    var c;
+
+    for (var b = 1; b < 1000; b++) {
+        a = (500000 - 1000 * b) / (1000 - b);
+        if (Math.floor(a) === a) {
+            c = 1000 - a - b;
+            break;
+        }
+    }
+    console.log(a*b*c);
+}
+
+triplet();
